63236daf6c9e9b8338c7fe36ca0453b365f1d998
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2021-05-24 19:31:45 +0200

    [Map extractor] Added empty Visual Studio 2017 project.

---
+++ Map_Extractor/Main.c
@@ -0,0 +1,14 @@
+/** @file Main.c
+ * Extract as many data as possible from a Stealth Combat map file.
+ * @author Adrien RICCIARDI
+ */
+#include <Windows.h>
+
+//-------------------------------------------------------------------------------------------------
+// Public functions
+//-------------------------------------------------------------------------------------------------
+/** Entry point. */
+int main(int argc, char *argv[])
+{
+	return 0;
+}
